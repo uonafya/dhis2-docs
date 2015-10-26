a8f697f21a1c5c77f6129f8d00bb3eb0a3374e0e
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 11:34:08 +0100

    Minor adjustments to travis build.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -7,8 +7,10 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 	#Copy the static index file
 	cmd="mv target/site target/${TRAVIS_BRANCH}"
 	exec $cmd
+	echo $cmd
 	cmd2="cp .utility/index.html target/${TRAVIS_BRANCH}"
 	exec $cmd2
+	echo $cmd2
 	# go to the out directory and create a *new* Git repo
 	cd target/
 	git init
@@ -18,7 +20,9 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 	git config user.email "travis@dhis2.org"
 	#Add the .nojekyll file since we are not using it for now
 	touch .nojekyll
-	git add .
+	cmd3="git add ${TRAVIS_BRANCH}"
+	exec $cmd3
+	echo $cmd3
 	git commit -m "Deploy to GitHub Pages"
 	git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
