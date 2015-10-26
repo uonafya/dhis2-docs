2a49927c0c96916201a8cbc4c5e3887b7362955f
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 14:56:34 +0100

    More travis tweaks.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
-if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != "false"  ]]; then
-  echo -e '\n============== deploy will not be started (from the fork) ==============\n'
-  exit 0
-fi
- 
-# Choose necessary argument according to the current branch.
-if [[ $TRAVIS_BRANCH == '2.21' ]]; then
-    echo $1
-elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-    echo $2
-else
-    exit 0
-fi
-
-
 
 if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	set -e # exit with nonzero exit code if anything fails
 
 	#TODO Find a better solution for this
 	#Copy the static index file
-	cmd="mv target/site/en target/site/${TRAVIS_BRANCH}"
-        echo $cmd	
-	exec $cmd
+	mv target/site/en target/site/${TRAVIS_BRANCH}
 	cp .utility/index.html target/site/
 	# go to the out directory and create a *new* Git repo
 	cd target/site/
@@ -33,8 +16,8 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 	git config user.email "travis@dhis2.org"
 	#Add the .nojekyll file since we are not using it for now
 	touch .nojekyll
-	git add .
-	git commit -m "Deploy to GitHub Pages"
+	git add -A .
+	git commit -a -m "Deploy to GitHub Pages"
 	git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 fi
