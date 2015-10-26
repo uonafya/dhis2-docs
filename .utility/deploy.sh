8a0cd9e93425e1d48b76d73836a683c939801d62
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 14:19:56 +0100

    More adjustment to the travis build.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+f this is fork - just exit
+if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != "false"  ]]; then
+  echo -e '\n============== deploy will not be started (from the fork) ==============\n'
+  exit 0
+fi
+ 
+# Choose necessary argument according to the current branch.
+if [[ $TRAVIS_BRANCH == '2.21' ]]; then
+    echo $1
+elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+    echo $2
+else
+    exit 0
+fi
+
+
 
 if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	set -e # exit with nonzero exit code if anything fails
 
 	#TODO Find a better solution for this
 	#Copy the static index file
-	cmd="mv target/site/en target/site/$TRAVIS_BRANCH"
-	exec cmd
+	cmd="mv target/site/en target/site/${TRAVIS_BRANCH}"
+        echo $cmd	
+	exec $cmd
 	cp .utility/index.html target/site/
 	# go to the out directory and create a *new* Git repo
 	cd target/site/
