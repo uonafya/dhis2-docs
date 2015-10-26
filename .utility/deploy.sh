0a643703feec8e20f211eeae8e036d6d8a7b9c98
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 15:10:32 +0100

    Attempt to build master and selected forks with travis.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	set -e # exit with nonzero exit code if anything fails
 
 	#TODO Find a better solution for this
 	#Copy the static index file
-	mv target/site/en target/site/${TRAVIS_BRANCH}
-	cp .utility/index.html target/site/
+        mkdir ${HOME}/gh-pages/
+	cp -R target/site/en ${HOME}/gh-pages/${TRAVIS_BRANCH}
+	cp .utility/index.html ${HOME}/gh-pages/
 	# go to the out directory and create a *new* Git repo
-	cd target/site/
+	cd ${HOME}/gh-pages/ 
 	git init
 
 	# inside this git repo we'll pretend to be a new user
