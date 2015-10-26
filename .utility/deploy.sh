c1133b1917ffeb7318777e1f94ba20026ff15718
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 12:54:19 +0100

    Reverting travis deploy script. Not working.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -5,14 +5,9 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 
 	#TODO Find a better solution for this
 	#Copy the static index file
-	cmd="mv target/site target/${TRAVIS_BRANCH}"
-	exec $cmd
-	echo $cmd
-	cmd2="cp .utility/index.html target/${TRAVIS_BRANCH}"
-	exec $cmd2
-	echo $cmd2
+	cp .utility/index.html target/site/
 	# go to the out directory and create a *new* Git repo
-	cd target/
+	cd target/site/
 	git init
 
 	# inside this git repo we'll pretend to be a new user
@@ -20,9 +15,7 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 	git config user.email "travis@dhis2.org"
 	#Add the .nojekyll file since we are not using it for now
 	touch .nojekyll
-	cmd3="git add ${TRAVIS_BRANCH}"
-	exec $cmd3
-	echo $cmd3
+	git add .
 	git commit -m "Deploy to GitHub Pages"
 	git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
