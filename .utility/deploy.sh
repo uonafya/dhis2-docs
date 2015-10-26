7578cdfc9aca4aab10d29267abe732ac47fecf41
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-26 11:15:08 +0100

    Minor adjustments to travis build.

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -5,9 +5,10 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 
 	#TODO Find a better solution for this
 	#Copy the static index file
-	cmd = "mv target/site target/${TRAVIS_BRANCH}"
-	exec cmd
-	cp .utility/index.html target/
+	cmd="mv target/site target/${TRAVIS_BRANCH}"
+	exec $cmd
+	cmd2="cp .utility/index.html target/${TRAVIS_BRANCH}"
+	exec $cmd2
 	# go to the out directory and create a *new* Git repo
 	cd target/
 	git init
