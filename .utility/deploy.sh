ea278a0e90767233e73381c960063a9cdf70ad11
Mark Polak <mark@thedutchies.com>
2015-11-03 22:46:36 +0100

    Build script updates

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -3,24 +3,31 @@
 if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	set -e # exit with nonzero exit code if anything fails
 
-        cd ${HOME}
-        git clone -b gh-pages https://github.com/dhis2/dhis2-docs.git dhis2/dhis2-docs
-        rm -rf ${HOME}/gh-pages/${TRAVIS_BRANCH}
-        mkdir -p ${HOME}/gh-pages/${TRAVIS_BRANCH}
-        cp -R ${TRAVIS_BUILD_DIR}/target/site/en ${HOME}/gh-pages/${TRAVIS_BRANCH}
-	cp ${TRAVIS_BUILD_DIR}/.utility/index.html ${HOME}/gh-pages/
-        
-	# inside this git repo we'll pretend to be a new user
-        cd ${HOME}/gh-pages/
-        rm -rf .git
-	#Be sure to completely init the repo again
-	git init
-        git config user.name "Travis CI"
-	git config user.email "travis@dhis2.org"
-	#Add the .nojekyll file since we are not using it for now
-	touch .nojekyll
-	git add .
-	git commit -a -m "Deploy to GitHub Pages"
-	git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+    cd ${HOME}
+    # Grab the pages branch we want to push the updated docs to
+    git clone -b gh-pages https://github.com/dhis2/dhis2-docs.git dhis2/dhis2-docs
+    
+    # Start with a clean dir so we also account for deletions
+    rm -rf ${HOME}/gh-pages/${TRAVIS_BRANCH}
+    mkdir -p ${HOME}/gh-pages/${TRAVIS_BRANCH}
 
+    # Copy the build files from the build branch into the docs branch
+    cp -R ${TRAVIS_BUILD_DIR}/target/site/en ${HOME}/gh-pages/${TRAVIS_BRANCH}
+    # Add the landing pages for the github pages webpage
+	cp ${TRAVIS_BUILD_DIR}/.utility/index.html ${HOME}/gh-pages/
+
+    cd ${HOME}/gh-pages/
+	
+    # inside this git repo we'll pretend to be a new user
+    git config user.name "Travis CI"
+	git config user.email "travis@dhis2.org"
+	
+    # Add the .nojekyll file since we are not using it for now
+	touch .nojekyll
+
+    # Add change files
+	git add -u .
+
+	git commit -a -m "Build $TRAVIS_JOB_NUMBER from updates to $TRAVIS_REPO_SLUG"
+	git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 fi
