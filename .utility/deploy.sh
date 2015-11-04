07e1b0e7d0a08b2115cd63ba3f20b869a48aa6f7
Mark Polak <mark@thedutchies.com>
2015-11-04 12:20:35 +0100

    Format index.html and reinit gh-pages repo on build

--- .utility/deploy.sh
+++ .utility/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-	set -e # exit with nonzero exit code if anything fails
+    set -e # exit with nonzero exit code if anything fails
 
     cd ${HOME}
 
@@ -10,7 +10,7 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
 
     # Grab the pages branch we want to push the updated docs to
     git clone -b gh-pages https://github.com/dhis2/dhis2-docs.git gh-pages
-    
+
     # Start with a clean dir so we also account for deletions
     rm -rf ${HOME}/gh-pages/${TRAVIS_BRANCH}
     mkdir -p ${HOME}/gh-pages/${TRAVIS_BRANCH}
@@ -18,20 +18,24 @@ if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" ==
     # Copy the build files from the build branch into the docs branch
     cp -R ${TRAVIS_BUILD_DIR}/target/site/en ${HOME}/gh-pages/${TRAVIS_BRANCH}
     # Add the landing pages for the github pages webpage
-	cp ${TRAVIS_BUILD_DIR}/.utility/index.html ${HOME}/gh-pages/
+    cp ${TRAVIS_BUILD_DIR}/.utility/index.html ${HOME}/gh-pages/
 
     cd ${HOME}/gh-pages/
-	
+
+    # Re-init the git repo for gh-pages so we start clean
+    rm -rf .git
+    git init
+
     # inside this git repo we'll pretend to be a new user
     git config user.name "Travis CI"
-	git config user.email "travis@dhis2.org"
-	
+    git config user.email "travis@dhis2.org"
+
     # Add the .nojekyll file since we are not using it for now
-	touch .nojekyll
+    touch .nojekyll
 
     # Make sure we add all files
-	git add .; git add -u .
+    git add .; git add -u .
 
-	git commit -a -m "Build $TRAVIS_JOB_NUMBER from updates to $TRAVIS_BRANCH"
-	git push --quiet "https://${GITHUB_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+    git commit -a -m "Build $TRAVIS_JOB_NUMBER from updates to $TRAVIS_BRANCH"
+    git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
 fi
