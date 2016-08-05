f76c574d266b166a6956bf2716af41549332c772
Mark Polak <mark@thedutchies.com>
2016-08-05 12:16:30 +0200

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -5,9 +5,9 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_REPO_SLUG" == "dhis2/dhis2-docs" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]]; then
-    mvn -Ddocbkx.fopLogLevel=ERROR -Denforcer.fail=false -q package
+    mvn -T 1C -Ddocbkx.fopLogLevel=ERROR -Denforcer.fail=false -q package
   else
-    mvn -f pom-short.xml -Ddocbkx.fopLogLevel=ERROR -Denforcer.fail=false  package
+    mvn -T 1C -f pom-short.xml -Ddocbkx.fopLogLevel=ERROR -Denforcer.fail=false  package
   fi
 fi 
 
